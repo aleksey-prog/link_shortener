ab66bb405a8c40e0785b02a80e74fb70ccd04037
Aleksey <tarakan195@gmail.com>
2021-05-11 17:03:22 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ cursor = conn.cursor()
 cursor.execute('''CREATE TABLE if NOT EXISTS "users" ("id" INTEGER NOT NULL UNIQUE, "login"	TEXT NOT NULL UNIQUE, 
 "password" TEXT NOT NULL, PRIMARY KEY("id" AUTOINCREMENT));''')
 cursor.execute('''CREATE TABLE if NOT EXISTS "links" ("id" INTEGER NOT NULL UNIQUE, "login"	TEXT NOT NULL, 
-"link_source" TEXT, "link_short" TEXT, "flag" INTEGER NOT NULL DEFAULT 0, PRIMARY KEY("id" 
+"link_source" TEXT, "link_short" TEXT, "flag" INTEGER NOT NULL DEFAULT 0, "count" INTEGER DEFAULT 0, PRIMARY KEY("id" 
 AUTOINCREMENT));''')
 conn.commit()
 conn.close()
@@ -50,8 +50,9 @@ def add_link():
     if data[0][0] == hashlib.sha256(request.authorization['password'].encode('utf-8')).hexdigest():
         con = sq.connect('links.db')
         cur = con.cursor()
-        cur.execute('''INSERT INTO links (login, link_source, link_short) VALUES (?,?,?)''',
-                    (request.authorization['username'], request.form['link'], get_hash(request.form['link']),))
+        cur.execute('''INSERT INTO links (login, link_source, link_short, flag) VALUES (?,?,?,?)''',
+                    (request.authorization['username'], request.form['link'], get_hash(request.form['link']),
+                     request.form['flag'],))
         con.commit()
         con.close()
 
@@ -60,10 +61,36 @@ def add_link():
 def url_redirect(link_id):
     con = sq.connect('links.db')
     cur = con.cursor()
-    cur.execute('''SELECT link_source FROM links WHERE link_short = (?)''', (link_id,))
+    cur.execute('''SELECT link_source, count, flag, login FROM links WHERE link_short = (?)''', (link_id,))
     data = cur.fetchall()
+    link_redirect = data[0][0]
+    link_counter = data[0][1]
+    flag = data[0][2]
+    login = data[0][3]
+    if flag == 0:
+        cur.execute('''SELECT password FROM users WHERE login = (?)''', (request.authorization['username'],))
+        data1 = cur.fetchall()
+        if data1[0][0] == hashlib.sha256(request.authorization['password'].encode('utf-8')).hexdigest()\
+                and login == request.authorization['username']:
+            cur.execute('''UPDATE links SET count = ? WHERE link_short = (?)''', (link_counter + 1, link_id,))
+            con.commit()
+            con.close()
+            return redirect(link_redirect)
+    elif flag == 1:
+        cur.execute('''SELECT password FROM users WHERE login = (?)''', (request.authorization['username'],))
+        data2 = cur.fetchall()
+        if data2[0][0] == hashlib.sha256(request.authorization['password'].encode('utf-8')).hexdigest():
+            cur.execute('''UPDATE links SET count = ? WHERE link_short = (?)''', (link_counter + 1, link_id,))
+            con.commit()
+            con.close()
+            return redirect(link_redirect)
+    elif flag == 2:
+        cur.execute('''UPDATE links SET count = ? WHERE link_short = (?)''', (link_counter + 1, link_id,))
+        con.commit()
+        con.close()
+        return redirect(link_redirect)
+    con.commit()
     con.close()
-    return redirect(data[0][0])
 
 
 if __name__ == '__main__':
