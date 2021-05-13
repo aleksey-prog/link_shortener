d0c6c43612544ff100c3a442be359f652fcea1bf
Aleksey <tarakan195@gmail.com>
2021-05-13 09:41:56 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -20,6 +20,18 @@ def get_hash(lk):
     return urlsafe_b64encode(hashlib.sha1(str(lk).encode()).digest()).decode()[0:12]
 
 
+def login_check(username, user_password):
+    con = sq.connect('links.db')
+    cur = con.cursor()
+    cur.execute('''SELECT password FROM users WHERE login = (?)''', (username,))
+    data = cur.fetchall()
+    con.close()
+    if data[0][0] == hashlib.sha256(user_password.encode('utf-8')).hexdigest():
+        return True
+    else:
+        return False
+
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
@@ -42,12 +54,7 @@ def reg_user():
 
 @app.route('/add_link', methods=['GET', 'POST'])
 def add_link():
-    con = sq.connect('links.db')
-    cur = con.cursor()
-    cur.execute('''SELECT password FROM users WHERE login = (?)''', (request.authorization['username'],))
-    data = cur.fetchall()
-    con.close()
-    if data[0][0] == hashlib.sha256(request.authorization['password'].encode('utf-8')).hexdigest():
+    if login_check(request.authorization['username'], request.authorization['password']):
         con = sq.connect('links.db')
         cur = con.cursor()
         cur.execute('''INSERT INTO links (login, link_source, link_short, flag) VALUES (?,?,?,?)''',
@@ -67,40 +74,36 @@ def url_redirect(link_id):
     link_counter = data[0][1]
     flag = data[0][2]
     login = data[0][3]
+    con.close()
     if flag == 0:
-        cur.execute('''SELECT password FROM users WHERE login = (?)''', (request.authorization['username'],))
-        data1 = cur.fetchall()
-        if data1[0][0] == hashlib.sha256(request.authorization['password'].encode('utf-8')).hexdigest() \
+        if login_check(request.authorization['username'], request.authorization['password'])\
                 and login == request.authorization['username']:
+            con = sq.connect('links.db')
+            cur = con.cursor()
             cur.execute('''UPDATE links SET count = ? WHERE link_short = (?)''', (link_counter + 1, link_id,))
             con.commit()
             con.close()
             return redirect(link_redirect)
     elif flag == 1:
-        cur.execute('''SELECT password FROM users WHERE login = (?)''', (request.authorization['username'],))
-        data2 = cur.fetchall()
-        if data2[0][0] == hashlib.sha256(request.authorization['password'].encode('utf-8')).hexdigest():
+        if login_check(request.authorization['username'], request.authorization['password']):
+            con = sq.connect('links.db')
+            cur = con.cursor()
             cur.execute('''UPDATE links SET count = ? WHERE link_short = (?)''', (link_counter + 1, link_id,))
             con.commit()
             con.close()
             return redirect(link_redirect)
     elif flag == 2:
+        con = sq.connect('links.db')
+        cur = con.cursor()
         cur.execute('''UPDATE links SET count = ? WHERE link_short = (?)''', (link_counter + 1, link_id,))
         con.commit()
         con.close()
         return redirect(link_redirect)
-    con.commit()
-    con.close()
 
 
 @app.route('/view_links', methods=['GET', 'POST'])
 def view_link():
-    con = sq.connect('links.db')
-    cur = con.cursor()
-    cur.execute('''SELECT password FROM users WHERE login = (?)''', (request.authorization['username'],))
-    data = cur.fetchall()
-    con.close()
-    if data[0][0] == hashlib.sha256(request.authorization['password'].encode('utf-8')).hexdigest():
+    if login_check(request.authorization['username'], request.authorization['password']):
         con = sq.connect('links.db')
         cur = con.cursor()
         cur.execute('''SELECT link_source FROM links WHERE login = (?)''', (request.authorization['username'],))
@@ -112,18 +115,50 @@ def view_link():
 
 @app.route('/delete_link', methods=['GET', 'POST'])
 def delete_link():
-    con = sq.connect('links.db')
-    cur = con.cursor()
-    cur.execute('''SELECT password FROM users WHERE login = (?)''', (request.authorization['username'],))
-    data = cur.fetchall()
-    con.close()
-    if data[0][0] == hashlib.sha256(request.authorization['password'].encode('utf-8')).hexdigest():
+    if login_check(request.authorization['username'], request.authorization['password']):
         con = sq.connect('links.db')
         cur = con.cursor()
         cur.execute('''DELETE FROM links WHERE login = (?) AND link_source = (?)''',
                     (request.authorization['username'], request.form['link'],))
         con.commit()
         con.close()
+
+
+@app.route('/<user_name>/<link_id>')
+def get_link_username(user_name, link_id):
+    con = sq.connect('links.db')
+    cur = con.cursor()
+    cur.execute('''SELECT link_source, count, flag, login FROM links WHERE link_short = (?)''', (link_id,))
+    data = cur.fetchall()
+    link_redirect = data[0][0]
+    link_counter = data[0][1]
+    flag = data[0][2]
+    login = data[0][3]
+    con.close()
+    if flag == 0:
+        if login_check(request.authorization['username'], request.authorization['password']) \
+                and login == request.authorization['username']:
+            con = sq.connect('links.db')
+            cur = con.cursor()
+            cur.execute('''UPDATE links SET count = ? WHERE link_short = (?)''', (link_counter + 1, link_id,))
+            con.commit()
+            con.close()
+            return redirect(link_redirect)
+    elif flag == 1:
+        if login_check(request.authorization['username'], request.authorization['password']):
+            con = sq.connect('links.db')
+            cur = con.cursor()
+            cur.execute('''UPDATE links SET count = ? WHERE link_short = (?)''', (link_counter + 1, link_id,))
+            con.commit()
+            con.close()
+            return redirect(link_redirect)
+    elif flag == 2:
+        con = sq.connect('links.db')
+        cur = con.cursor()
+        cur.execute('''UPDATE links SET count = ? WHERE link_short = (?)''', (link_counter + 1, link_id,))
+        con.commit()
+        con.close()
+        return redirect(link_redirect)
 
 
 if __name__ == '__main__':
